faf3128b34f978987f28216cf10283224bbc81b4
Anthony Papillon <github@perhonen.fr>
2015-07-25 22:23:35 +0200

    passage de pyexiv2 à GExiv2

--- src/eos_digital_import.py
+++ src/eos_digital_import.py
@@ -4,9 +4,10 @@
 import datetime
 import logging
 import os
-import pyexiv2
 import shutil
 import sys
+
+from gi.repository import GExiv2
 
 source = sys.argv[1]
 target = sys.argv[2]
@@ -22,78 +23,22 @@ def get_files(path):
             files.extend(get_files(filename))
         else:
             files.append(filename)
-    files.sort()
     return files
 
-def cmp_file(x, y, bufSize=512):
-    """Compare les 2 fichiers et renvoie True seulement s'ils ont un contenu identique"""
-    f1 = f2 = None
-    res = False
+def rename(src, target):
     try:
-        if os.path.getsize(x) == os.path.getsize(y):
-            f1 = open(x, "rb")
-            f2 = open(y, "rb")
-            while True:
-                buf1 = f1.read(bufSize)
-                if len(buf1) == 0:
-                    res = True
-                    break
-                buf2 = f2.read(bufSize)
-                if buf1 != buf2:
-                    break
-            f1.close()
-            f2.close()
-    except:
-        if f1 != None: f1.close()
-        if f2 != None: f2.close()
-        raise
-    return res
-
-def getdate(src):
-    res = False
-
-    try:
-        img = pyexiv2.metadata.ImageMetadata(src)
-        img.read()
+        exif = GExiv2.Metadata(src)
     except IOError:
-        img = False
+        return False
 
-    if img:
-        for k in ['Exif.Photo.DateTimeOriginal', 'Exif.Photo.DateTimeDigitized',
-                  'Exif.Image.DateTime', 'Xmp.exif.DateTimeOriginal']:
-            if k in img:
-                date = img[k]
-                res = date.value
-                break
+    try:
+        exifdate = exif.get_tag_string('Exif.Photo.DateTimeOriginal')
+        if exifdate == None:
+            return False
+        date = datetime.datetime.strptime(exifdate, '%Y:%m:%d %H:%M:%S')
+    except KeyError:
+        return False
 
-    if res == False:
-        spFilename = os.path.splitext(src)
-        infoFile = '%s.%s' % (spFilename[0], 'THM')
-        if os.path.lexists(infoFile) and spFilename[1].upper() != '.THM':
-            res = getdate(infoFile)
-            print res
-
-    if res == False:
-        bnFilename = os.path.splitext(os.path.basename(src))[0]
-        if bnFilename.startswith('VID_') or bnFilename.startswith('IMG_'):
-            bnFilename = bnFilename[4:]
-        try:
-            res = datetime.datetime.strptime(bnFilename[:15], '%Y%m%d_%H%M%S')
-        except ValueError:
-            res = False
-
-    if res == False:
-        res = datetime.datetime.utcfromtimestamp(os.stat(src).st_mtime)
-        r = raw_input('file %s date = %s ? [Y/n]' % (src, res))
-        if r and r.upper() != 'Y':
-            r_date = raw_input('Enter date file (format: 2000-12-31 12:00:00): ')
-            res = datetime.datetime.strptime(r_date, '%Y-%m-%d %H:%M:%S')
-        elif not r:
-            res = False
-
-    return res
-
-def cpy(src, outpath, date):
     destination = os.path.join(target, date.strftime('%Y/%m/%d'))
     if not os.path.lexists(destination):
         os.makedirs(destination)
@@ -101,25 +46,28 @@ def cpy(src, outpath, date):
     ext = os.path.splitext(src)[-1]
     for index in xrange(1, 255):
         filename = date.strftime('%Y%m%d_%H%M%S' + '_%03d%s' %(index, ext))
-        filename = os.path.join(destination, filename.upper())
+        filename = os.path.join(destination, filename)
         if not os.path.lexists(filename):
             shutil.copy2(src, filename)
-            return filename
-
-        elif cmp_file(src, filename):
             return filename
 
     return False
 
 for file in get_files(source):
     print 'file:', file
-    date = getdate(file)
+    res = rename(file, target)
+    if res:
+        logging.debug('%s copied in %s.' % (file, res))
+#        os.remove(file)
 
-    if date:
-        newname = cpy(file, target, date)
-        logging.debug('%s copied in %s.' % (file, newname))
-        os.remove(file)
-        
+    elif os.path.splitext(file)[-1].upper() in ('.MOV', '.MP4'):
+        mtime = os.stat(file).st_mtime
+        date = datetime.datetime.utcfromtimestamp(mtime)
+        dst = os.path.join(target, date.strftime('%Y/%m/%d/%Y%m%d_%H%M%S' + '_%s' %(os.path.split(file)[1])))
+        shutil.copy2(file, dst)
+        logging.debug('%s copied in %s.' % (file, dst))
+#        os.remove(file)
+
     else:
         logging.warn('%s failed.' % (file))
 
